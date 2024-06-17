52f019b0001dcb81b842a9641ae27e5ad7f805a7
juanabernstein <jbernstein@udesa.edu.ar>
2024-06-17 11:22:40 -0300

    creacion de seccion y comienzo en js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
+let url = 'https://fakestoreapi.com/products/category/jewelery' ;
+fetch (url) 
+.then (function(response) {
+    return response.json();
+})
+.then (function (data){})
+.catch (function (error) {
+    console.log ("El error es" + error);
+})
 
