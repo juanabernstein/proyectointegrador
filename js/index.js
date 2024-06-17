38995482bcbc2b1482492da5e089ad8b204a9a99
juanabernstein <jbernstein@udesa.edu.ar>
2024-06-17 15:49:34 -0300

    home seccion jewlery

--- js/index.js
+++ js/index.js
@@ -3,7 +3,30 @@ fetch (url)
 .then (function(response) {
     return response.json();
 })
-.then (function (data){})
+.then (function (data) {
+    let prod = document.querySelector(".containerSection1") ;
+    let accesorios = '';
+    let info = data;
+    
+    
+    for(let i=0; i<info.length; i++ ) {
+        console.log (info[i]);
+        accesorios += `<article class="bolsodecuero">
+        <h3>${info[i].title}</h3>
+        <div class="containerImg">
+            <a href="detalleProducto.html?id=${info[i].id}">
+                <img src="${info[i].image}" alt="">
+            </a>
+        </div>
+        <p> ${info[i].description}</p>
+        <h4 class="precio"> ${info[i].price}</h4>
+        <a class="buttonVerMas" href="detalleProducto.html?id=${info[i].id}">Ver mas</a>
+    </article>`;
+    }
+
+    prod.innerHTML = accesorios ; 
+
+})
 .catch (function (error) {
     console.log ("El error es" + error);
 })
