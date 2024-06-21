7d618c678f54cd46e64eab669de15c2a17d25d01
juanabernstein <jbernstein@udesa.edu.ar>
2024-06-21 17:43:55 -0300

    validaciones login

--- js/login.js
+++ js/login.js
@@ -2,3 +2,26 @@ let logform = document.querySelector ('#loginForm') ;
 let email = document.querySelector ('#loginEmail') ; 
 let pass = document.querySelector ('#loginPassword') ;  
 
+logform.addEventListener ('submit', function(event){
+    event.preventDefault();
+
+    if (email.value === "") {
+        alert('Porfavor complete el campo email!');
+        return;
+    }
+
+    if (pass.value === "") {
+    alert('Por favor complete el campo contraseña');
+    return;
+    } 
+
+    if (pass.value.length <6) {
+        alert('La contraseña debe tener al menos 6 caracteres');
+        return;
+    }
+   
+
+})
+
+
+
