71b18775a8bf25749e80793a5cc7077b61cc8295
delficamuglia <dcamuglia@udesa.edu.ar>
2024-06-25 00:20:20 -0300

    modificando el saludo

--- js/saludo.js
+++ js/saludo.js
@@ -1,5 +1,6 @@
-document.getElementsByClassName("quitar")[0].style.display = "none";
-document.getElementsByClassName("quitar")[1].style.display = "none";
+
+document.querySelector(".quitar")[0].style.display = "none";
+document.querySelector(".quitar")[1].style.display = "none";
 
 document.getElementById("titulo").innerHTML = "Bienvenido" + localStorage.getItem ('#user')
 
