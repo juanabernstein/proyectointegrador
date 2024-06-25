4d70939194877624e66d0cec257e2e20e3cd3e5f
Maguisanchezudesa <msanchez@udesa.edu.ar>
2024-06-24 23:06:13 -0300

    logout y cambie algo del hombre js

--- js/logout.js
+++ js/logout.js
@@ -10,6 +10,33 @@ if (datosParseUserLogout){
     })
     .then (function(data){
         console.log("data", data)
-        let
+        let headerHome = document.querySelector ('.listaheader')
     })
+    
 }
+
+headerHome.innerHTML =
+
+`<div><h2>"bienvenido ${datosParseUserLogout}"</h2></div>
+<div class = listaHeader>
+<li<a href="./index.html?">Home</a></li>
+<li<a href="./categoria.html?categoria=${arrayCategoria.category ="jewelry"}>Accesorios</a></li>
+<li<a href="./categoria.html?categoria=${arrayCategoria.category ="electronica"}>Electronica</a></li>
+<li<a href="./categoria.html?categoria=${arrayCategoria.category ="men's clothing"}>Hombre</a></li>
+<li<a href="./categoria.html?categoria=${arrayCategoria.category ="women's clothing"}>Mujer</a></li>
+<li<a href="./cart.html?">Carrito</a></li>
+<li id="logout"<a href="">Logout</a></li>
+
+</div>`
+
+    document.getElementById('logout').addEventListener('click', 
+    function (event){
+        event.preventDefault();
+        localStorage.clear();
+        alert("Cerraste sesion");
+        window.location.href = './index.html'
+        console.log(datosParseUserLogout)
+     .catch(function (e){
+            console.log("error1", e)
+        })
+    });
